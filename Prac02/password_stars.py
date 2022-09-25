833f53b51a62eb08cef973317d6ab95e14c78dee
Bryce-Fern <bryce.fern@my.jcu.edu.au>
2022-09-25 19:42:55 +1000

    add while loop to check for minimum length of password

--- Prac02/password_stars.py
+++ Prac02/password_stars.py
@@ -1,3 +1,15 @@
 MIN_Length = 4
 print("Please enter a password of at least", MIN_Length, "Characters")
 get_password = str(input("Enter Password: "))
+num_characters = len(get_password)
+
+while num_characters < MIN_Length:
+    print("Password doesn't meet minimum length")
+    print("Please enter a password of at least", MIN_Length, "Characters")
+    get_password = str(input("Enter Password: "))
+    num_characters = len(get_password)
+else:
+    for i in range(num_characters):
+        print('*', end=' ')
+    print()
+
