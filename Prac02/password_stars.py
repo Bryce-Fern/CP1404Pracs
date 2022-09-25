ec5b3e3408081a04c002c92d13f0abb3c717f112
Bryce-Fern <bryce.fern@my.jcu.edu.au>
2022-09-25 19:49:33 +1000

    refactor get_password

--- Prac02/password_stars.py
+++ Prac02/password_stars.py
@@ -1,15 +1,19 @@
 MIN_Length = 4
-print("Please enter a password of at least", MIN_Length, "Characters")
-get_password = str(input("Enter Password: "))
-num_characters = len(get_password)
+
+
+def get_password():
+    global password, num_characters
+    print("Please enter a password of at least", MIN_Length, "Characters")
+    password = str(input("Enter Password: "))
+    num_characters = len(password)
+
+
+get_password()
 
 while num_characters < MIN_Length:
     print("Password doesn't meet minimum length")
-    print("Please enter a password of at least", MIN_Length, "Characters")
-    get_password = str(input("Enter Password: "))
-    num_characters = len(get_password)
+    get_password()
 else:
     for i in range(num_characters):
         print('*', end=' ')
     print()
-
