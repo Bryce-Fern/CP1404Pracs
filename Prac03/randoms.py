f23c2f5b6ff188d6ef4df7cac08fac9d6884aa3e
Bryce Fern <bryce.fern@my.jcu.edu.au>
2022-10-02 21:16:33 +1000

    add code to print random number between 1 and 100

---
+++ Prac03/randoms.py
@@ -0,0 +1,13 @@
+# What did you see on line 1? - 9
+# What was the smallest number you could have seen, what was the largest? - Smallest: 5 Largest: 20
+
+# What did you see on line 2? - 5
+# What was the smallest number you could have seen, what was the largest? - Smallest: 3 Largest: 9
+# Could line 2 have produced a 4? No, it jumps every 2 numbers and starts at 3, therefore it skips 4
+
+# What did you see on line 3? - 2.5040014966879593
+# What was the smallest number you could have seen, what was the largest? Smallest: 2.5000 Largest: 5.5000
+
+import random
+
+print(random.randint(1, 100))
