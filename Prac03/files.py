9f23d13fb2002c40b74bf3933d18961981a82505
Bryce Fern <bryce.fern@my.jcu.edu.au>
2022-10-02 22:27:43 +1000

    Trial reading and writing in txt files and printing integers and strings from the files into a python program

---
+++ Prac03/files.py
@@ -0,0 +1,29 @@
+OUTPUT_FILE = "name.txt"
+out_file = open(OUTPUT_FILE, 'w')
+
+username = str(input("What is your name?: "))
+print(username, file=out_file)
+out_file.close()
+
+name = open("name.txt", 'r')
+username = name.read()
+name.close()
+print("Your name is", username)
+
+numbers_file = open("numbers.txt", 'r')
+number1 = int(numbers_file.readline())
+number2 = int(numbers_file.readline())
+numbers_file.close()
+print(number1 + number2)
+
+# Is there a way to use one readlines variable instead of using two readline variables to get both numbers and
+# then sum the two numbers and print that sum variable? Apologies if this makes no sense when reading, I will ask
+# in the next practical.
+
+numbers_file = open("numbers.txt", 'r')
+total = 0
+for numbers in numbers_file:
+    number = int(numbers)
+    total += number
+numbers_file.close()
+print(total)
