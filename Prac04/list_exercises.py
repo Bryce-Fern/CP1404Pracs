fa201a7cf5e8af4c267730eb81b750b7b12a7152
Bryce Fern <bryce.fern@my.jcu.edu.au>
2022-10-12 01:16:50 +1000

    Add usernames list and ask for username. Check if username is in usernames

--- Prac04/list_exercises.py
+++ Prac04/list_exercises.py
@@ -6,8 +6,6 @@ def main():
             numbers.append(number)
         except ValueError:
             print("Invalid input")
-            number = int(input("Number: "))
-            numbers.append(number)
 
     print(f"The first number is {numbers[0]}")
     print(f"The last number is {numbers[-1]}")
@@ -15,5 +13,15 @@ def main():
     print(f"The largest number is {max(numbers)}")
     print(f"The average of the numbers is {sum(numbers) / len(numbers)}")
 
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = str(input("What if your username?: "))
+
+if username in usernames:
+    print("Access Granted")
+else:
+    print("Access Denied")
 
 main()
