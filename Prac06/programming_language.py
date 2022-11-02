c6faaa573b1f34fc1bd3173cd7b3d7dc08f672e0
Bryce Fern <bryce.fern@my.jcu.edu.au>
2022-11-02 21:48:22 +1000

    write a class to print a programming language's details

---
+++ Prac06/programming_language.py
@@ -0,0 +1,13 @@
+class ProgrammingLanguage:
+
+    def __init__(self, language="", dynamic="", reflection="", year=0):
+        self.language = language
+        self.dynamic = dynamic
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        self.dynamic = "Dynamic"
+
+    def __str__(self):
+        return f"{self.language}, {self.dynamic} Typing, Reflection={self.reflection}, First appeared in {self.year}"
