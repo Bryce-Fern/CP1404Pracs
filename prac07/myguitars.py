c836e7e03e4df72e210118bc74a84f5ebd570b79
Bryce Fern <bryce.fern@my.jcu.edu.au>
2022-11-10 11:47:56 +1000

    print guitar details and test sorting function.

---
+++ prac07/myguitars.py
@@ -0,0 +1,28 @@
+class Guitars:
+
+    def __init__(self, name, year, cost):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __repr__(self):
+        return f"{self.name} first introduced in the year: {self.year}, Cost: ${self.cost}"
+
+
+def run_test():
+    guitars = []
+    in_file = open('guitars.csv', 'r')
+    in_file.readline()
+    for line in in_file:
+        parts = line.strip().split(',')
+        guitar = Guitars(parts[0], int(parts[1]), (parts[2]))
+        guitars.append(guitar)
+    in_file.close()
+
+    for guitar in guitars:
+        guitars.sort()
+        print(guitar)
+
+
+if __name__ == "__main__":
+    run_test()
